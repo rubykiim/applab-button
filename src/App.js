e08897a8d30651271a5912716552bb3fabd09869
Ruby <IKID@gimseung-aui-MacBook-Pro.local>
2019-11-12 22:43:44 +0800

    medium level

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [active, setActive] = useState("");
+
+  function colorChange(e) {
+    let id = e.target.id;
+    setActive(id);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Buttons">
+        <Button
+          isActive={active == "First Button" ? true : false}
+          nthbutton={colorChange}
+          name="First Button"
+        />
+        <Button
+          isActive={active == "Second Button" ? true : false}
+          nthbutton={colorChange}
+          name="Second Button"
+        />
+        <Button
+          isActive={active == "Third Button" ? true : false}
+          nthbutton={colorChange}
+          name="Third Button"
+        />
+      </div>
+      <div>
+        <ContentBoxes id={active} />
+      </div>
+    </div>
+  );
+}
+
+function Button(props) {
+  return (
+    <div>
+      <button
+        className={props.isActive ? "color" : ""}
+        id={props.name}
+        onClick={props.nthbutton}>
+        {props.name}
+      </button>
+    </div>
+  );
+}
+
+function ContentBoxes(props) {
+  return (
+    <div className="contentbox">
+      <p className="content">You have selected: {props.id}</p>
     </div>
   );
 }
